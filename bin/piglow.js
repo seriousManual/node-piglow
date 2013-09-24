75bbee3dc8bc3e37d582f7057402a73d824bd311
zaphod1984 <zaphod84@gmx.de>
2013-09-24 16:02:02 +0200

    ensuring that properties exist

--- bin/piglow.js
+++ bin/piglow.js
@@ -3,19 +3,23 @@
 var argv = require('optimist').argv;
 var piGlow = require('../');
 
-createInterface(argv, function(error, interface) {
+createInterface(argv, function(error, piGlowInterface) {
    if(error) {
        console.error('could not create interface: ', error.message);
        return;
    }
 
-    Object.keys(argv).forEach(function(key) {
-        interface[key] = argv[key];
-    });
+   Object.keys(argv).forEach(function(key) {
+       if(Object.hasOwnProperty.call(piGlowInterface, key)) {
+           piGlowInterface[key] = argv[key];
+       }
+   });
 });
 
 function createInterface(options, callback) {
     if(options.mocked === true) {
+        delete options.mocked;
+
         var myMock = new piGlow.BackendMock();
         var myInterface = piGlow.piGlowInterface.create(myMock);
 
