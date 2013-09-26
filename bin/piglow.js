8ef3b7de2dc5d41afae039c2564b8c6feb6c23fb
zaphod1984 <zaphod84@gmx.de>
2013-09-26 10:19:04 +0200

    code style

--- bin/piglow.js
+++ bin/piglow.js
@@ -12,28 +12,31 @@ if(argv.h || argv.help) {
 }
 
 createInterface(argv, function(error, piGlowInterface) {
-   if(error) {
-       console.error('could not create interface: ', error.message);
-       process.exit(1);
-   }
+    if(error) {
+        console.error('could not create interface: ', error.message);
+        process.exit(1);
+    }
 
-   piGlowInterface.startTransaction();
-   Object.keys(argv).forEach(function(key) {
-       var value = argv[key];
+    piGlowInterface.startTransaction();
 
-       if(Object.hasOwnProperty.call(piGlowInterface, key)) {
-           if(value === true) {
-               //shorthand versions
-               piGlowInterface[key];
-           } else {
-               piGlowInterface[key] = value;
-           }
-       } else {
-           console.error('Unknown parameter: ', key);
-           process.exit(1);
-       }
-   });
-   piGlowInterface.commitTransaction();
+    Object.keys(argv).forEach(function(key) {
+        var value = argv[key];
+
+        if(Object.hasOwnProperty.call(piGlowInterface, key)) {
+            if(value === true) {
+                //shorthand versions
+                piGlowInterface[key];
+            } else {
+                piGlowInterface[key] = value;
+            }
+        } else {
+            console.error('Unknown parameter: ', key);
+            process.exit(1);
+        }
+    });
+
+    piGlowInterface.commitTransaction();
+
 });
 
 function createInterface(options, callback) {
