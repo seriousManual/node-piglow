b067d46766fe76e949da445dd9901923b971a290
zaphod1984 <zaphod84@gmx.de>
2013-10-11 16:35:52 +0200

    animations updates

--- examples/animation.js
+++ examples/animation.js
@@ -1,46 +1,27 @@
 var animation = require('../lib/animations/index');
 var pi = require('../lib/interface');
 
+var piGlowBackendMock = {
+    startTransaction: function() {
+        console.log('start Transaction');
+    },
+    commitTransaction: function() {
+        console.log('commit');
+    }
+};
+
+animation({}, piGlowBackendMock)
+    .fade().to(pi({ring_0:true})).in('2s').parallel()
+    .fade().after('1s').to(pi({ring_1:true})).in('2s').parallel()
+    .fade().after('2s').to(pi({ring_2:true})).in('2s').parallel()
+    .repeat(3)
+    .start(function() {
+        console.log('i looped 3 times');
+    });
+
 //var a = animation().set(1)
 //    .fade().after('2s').in('100ms').to(2)
 //    .fade().after('2s').in('100ms').to(3);
 //
 //a.start();
 //a.stop();
-
-animation()
-    .set(pi({ring_0:true}))
-    .set().after('2s').to(pi({ring_1:true}))
-    .set().after('2min').to(pi({ring_1:true}))
-    .set().after('0.2s').to(pi({ring_2:true}))
-    .repeat(3)
-    .start(function() {
-        console.log('i looped 3 times');
-    });
-
-//animation()
-//    .set(1)
-//    .set().after(2).to(2)
-//    .set().after(2).to(3)
-//    .repeat('3times')
-//    .start(function() {
-//        console.log('i looped 3 times');
-//    });
-//
-//animation()
-//    .set(1)
-//    .set().after(2).to(2)
-//    .set().after(2).to(3)
-//    .repeat('3s')
-//    .start(function() {
-//        console.log('i looped for 3 seconds');
-//    });
-//
-//animation()
-//    .set(1)
-//    .set().after(2).to(2)
-//    .set().after(2).to(3)
-//    .repeat('2ms')
-//    .start(function() {
-//        console.log('i looped for 2 ms');
-//    });
