4a6ae4e0c918f4a44c1f71589f746a75405dd0ad
zaphod1984 <zaphod84@gmx.de>
2013-10-13 00:02:05 +0200

    animation tunings

--- examples/animation.js
+++ examples/animation.js
@@ -5,20 +5,19 @@ var piGlowBackendMock = {
     writeBytes: function() {}
 };
 
-animation({debug: true}, piGlowBackendMock)
-        .set().to(pi(['leg_0'])).after('1s')
-        .set().to(pi(['leg_1'])).after('1s')
-        .set().to(pi(['leg_2'])).after('1s')
-        .start();
+//animation({debug: true}, piGlowBackendMock)
+//        .set().to(pi(['ring_1'])).after('3s')
+//        .set().to(pi(['ring_5'])).after('3s')
+//        .start();
 
-//animation({debug:true, interval: 100}, piGlowBackendMock)
-//        .fade().after('1s').to(pi({ring_0: 255})).in('1s')
-//        .fade().after('1s').to(pi({ring_1: 255})).in('1s')
-//        .fade().after('1s').to(pi({ring_2: 255})).in('1s')
-//        .fade().after('1s').to(pi({ring_3: 255})).in('1s')
-//        .fade().after('1s').to(pi({ring_4: 255})).in('1s')
-//        .fade().after('1s').to(pi({ring_5: 255})).in('1s')
-//        .repeat('3times')
-//        .start(function() {
-//            console.log('i looped 3 times');
-//        });
+animation({debug:true, interval: 100}, piGlowBackendMock)
+        .fade().after('1s').to(pi({ring_0: 255})).in('1s')
+        .fade().after('1s').to(pi({ring_1: 255})).in('1s')
+        .fade().after('1s').to(pi({ring_2: 255})).in('1s')
+        .fade().after('1s').to(pi({ring_3: 255})).in('1s')
+        .fade().after('1s').to(pi({ring_4: 255})).in('1s')
+        .fade().after('1s').to(pi({ring_5: 255})).in('1s')
+        .repeat('3times')
+        .start(function() {
+            console.log('i looped 3 times');
+        });
