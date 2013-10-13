c961cc6dfb96d123e674a007be1a1903375ae626
zaphod1984 <zaphod84@gmx.de>
2013-10-13 15:54:19 +0200

    debug parameter

--- examples/animations/janePiglow.js
+++ examples/animations/janePiglow.js
@@ -1,12 +1,13 @@
+var args = require('optimist').argv;
 var animation = require('../../lib/animations/index');
 var pi = require('../../lib/interface');
-
 
 var PiGlowBackend = require('../../lib/PiGlowBackend');
 var myBackend = new PiGlowBackend();
 
+var debug = args.debug || args.d || false;
 
-var rings = animation({debug: false, interval:10}, myBackend)
+var rings = animation({debug: debug, interval:10}, myBackend)
         .set().to(pi(['ring_0'])).after('0.03s')
         .set().to(pi(['ring_1'])).after('0.03s')
         .set().to(pi(['ring_2'])).after('0.03s')
@@ -15,7 +16,7 @@ var rings = animation({debug: false, interval:10}, myBackend)
         .set().to(pi(['ring_5'])).after('0.03s')
         .repeat('5times');
 
-var legs = animation({debug: false, interval: 10}, myBackend)
+var legs = animation({debug: debug, interval: 10}, myBackend)
         .set().to(pi(['leg_0'])).after('0.03s')
         .set().to(pi(['leg_1'])).after('0.03s')
         .set().to(pi(['leg_2'])).after('0.03s')
