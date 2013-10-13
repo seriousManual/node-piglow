9b91e531cb8f7298c2462950b4aad9407463c4d9
zaphod1984 <zaphod84@gmx.de>
2013-10-13 15:49:38 +0200

    some example stuff

--- examples/animations/janePiglow.js
+++ examples/animations/janePiglow.js
@@ -6,25 +6,25 @@ var PiGlowBackend = require('../../lib/PiGlowBackend');
 var myBackend = new PiGlowBackend();
 
 
-var rings = animation({debug: true}, myBackend)
-        .set().to(pi(['ring_0'])).after('0.1s')
-        .set().to(pi(['ring_1'])).after('0.1s')
-        .set().to(pi(['ring_2'])).after('0.1s')
-        .set().to(pi(['ring_3'])).after('0.1s')
-        .set().to(pi(['ring_4'])).after('0.1s')
-        .set().to(pi(['ring_5'])).after('0.1s')
-        .repeat('1times');
+var rings = animation({debug: false, interval:10}, myBackend)
+        .set().to(pi(['ring_0'])).after('0.03s')
+        .set().to(pi(['ring_1'])).after('0.03s')
+        .set().to(pi(['ring_2'])).after('0.03s')
+        .set().to(pi(['ring_3'])).after('0.03s')
+        .set().to(pi(['ring_4'])).after('0.03s')
+        .set().to(pi(['ring_5'])).after('0.03s')
+        .repeat('5times');
 
-var legs = animation({debug: true}, myBackend)
-        .set().to(pi(['leg_0'])).after('0.1s')
-        .set().to(pi(['leg_1'])).after('0.1s')
-        .set().to(pi(['leg_2'])).after('0.1s')
-        .repeat('1times');
+var legs = animation({debug: false, interval: 10}, myBackend)
+        .set().to(pi(['leg_0'])).after('0.03s')
+        .set().to(pi(['leg_1'])).after('0.03s')
+        .set().to(pi(['leg_2'])).after('0.03s')
+        .repeat('5times');
 
 animation.jane()
         .chain(rings)
         .chain(legs)
-        .repeat('1times')
+        .repeat('5times')
         .start(function() {
             console.log( 'chain finished' );
         });
