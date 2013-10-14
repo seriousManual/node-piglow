626a4d0efa31d3497cf9dac4bdb4d5174b5b7ddf
zaphod1984 <zaphod84@gmx.de>
2013-10-14 08:53:37 +0200

    test added

---
+++ tests/utilHrtime.tests.js
@@ -0,0 +1,16 @@
+var expect = require('chai').expect;
+
+var hrtime = require('../lib/util/hrtime');
+
+describe('hrtime', function() {
+
+    it('should return the correct time', function(done) {
+        var elapsed = hrtime();
+
+        setTimeout(function() {
+            expect(elapsed()).within(100, 120);
+            done();
+        }, 100);
+    });
+
+});
