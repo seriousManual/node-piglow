a2ffc75491a7284aea74a5d26c9d9a222a74301c
Manuel Ernst <zaphod84@gmx.de>
2013-10-20 01:53:21 +0200

    Update animation.js

--- examples/animations/animation.js
+++ examples/animations/animation.js
@@ -2,7 +2,9 @@ var animation = require('../../lib/animations/index');
 var pi = require('../../lib/interface');
 
 var piGlowBackendMock = {
-    writeBytes: function() {}
+    writeBytes: function(bytes) {
+        console.log(bytes);
+    }
 };
 
 animation({interval:10, debug: true}, piGlowBackendMock)
@@ -12,13 +14,11 @@ animation({interval:10, debug: true}, piGlowBackendMock)
     .set().to(pi(['ring_3'])).after('0.1s')
     .set().to(pi(['ring_4'])).after('0.1s')
     .set().to(pi(['ring_5'])).after('0.1s')
-    .repeat('5s')
+    .fade().after('100ms').to(pi(['ring_4'])).in('0.3s')
+    .fade().after('100ms').to(pi(['ring_3'])).in('0.3s')
+    .fade().after('100ms').to(pi(['ring_2'])).in('0.3s')
+    .fade().after('100ms').to(pi(['ring_1'])).in('0.3s')
+    .fade().after('100ms').to(pi(['ring_0'])).in('0.3s')
+    .fade().after('100ms').to(pi(['reset'])).in('0.3s')
+    .repeat(3)
     .start();
-
-//var a = animation({debug:true, interval: 100}, piGlowBackendMock)
-//        .fade().after('2s').to(pi({ring_0: 255})).in('1s')
-//        .fade().after('2s').to(pi({ring_1: 255})).in('1s')
-//        .repeat(1)
-//        .start(function() {
-//            console.log('its over...')
-//        });
