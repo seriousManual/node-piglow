36e4c765ed7f0198dbe56ccc96662bf8282fbc6d
zaphod1984 <zaphod84@gmx.de>
2014-02-19 15:47:28 +0100

    improved tests

--- tests/piglowBackend.tests.js
+++ tests/piglowBackend.tests.js
@@ -1,22 +1,20 @@
 var expect = require('chai').expect;
 var sand = require('sandboxed-module');
 
-function createI2cMock() {
-    return function (address, options) {
-        this._addr = address;
-        this._options = options;
-        this._adresses = [];
-        this._written = [];
+function I2cMock(address, options) {
+    this._addr = address;
+    this._options = options;
+    this._adresses = [];
+    this._written = [];
 
-        this.setAddress = function(address) {
-            this._adresses.push(address);
-        };
+    this.setAddress = function(address) {
+        this._adresses.push(address);
+    };
 
-        this.writeBytes = function(address, values, callback) {
-            this._written.push([address, values]);
+    this.writeBytes = function(address, values, callback) {
+        this._written.push([address, values]);
 
-            setImmediate(callback);
-        };
+        setImmediate(callback);
     };
 }
 
@@ -24,15 +22,13 @@ describe('piGlowBackend', function() {
     it('should initialize correctly', function(done) {
         var Backend = sand.require('../lib/PiGlowBackend', {
             requires: {
-                'i2c': createI2cMock()
+                'i2c': I2cMock
             }
         });
 
-        var i = new Backend();
-
-        i
+        new Backend()
             .on('initialize', function() {
-                var data = i._wire;
+                var data = this._wire;
 
                 expect(data._addr).to.equal(84);
                 expect(data._options).to.deep.equal({device: '/dev/i2c-1'});
@@ -49,7 +45,7 @@ describe('piGlowBackend', function() {
     it('should initialize and write bytes', function(done) {
         var Backend = sand.require('../lib/PiGlowBackend', {
             requires: {
-                'i2c': createI2cMock()
+                'i2c': I2cMock
             }
         });
 
