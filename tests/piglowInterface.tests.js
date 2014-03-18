730a1d6d501008262b8c917b8a795a4e18125aa5
zaphod1984 <zaphod84@gmx.de>
2014-03-18 01:09:33 +0100

    rearranged interface tests

--- tests/piglowInterface.tests.js
+++ tests/piglowInterface.tests.js
@@ -20,7 +20,7 @@ describe('interface', function() {
         console.log( 'aaaa' );
     });
 
-    describe.only('single leds', function() {
+    describe('single leds', function() {
         it('l_0_0 (shorthand)', function() {
             ti.l_0_0;
             expect(ti.values).to.deep.equal({
@@ -258,245 +258,256 @@ describe('interface', function() {
         });
     });
 
-    it('leg_0 (shorthand)', function() {
-        ti.leg_0;
-        expect(ti.values).to.deep.equal([255,255,255,255,0,0,0,0,0,0,0,0,255,0,255,0,0,0]);
-    });
-
-    it('leg_0', function() {
-        ti.leg_0 = 100;
-        expect(ti.values).to.deep.equal([8,8,8,8,0,0,0,0,0,0,0,0,8,0,8,0,0,0]);
-    });
-
-    it('leg_1 (shorthand)', function() {
-        ti.leg_1;
-        expect(ti.values).to.deep.equal([0,0,0,0,255,255,255,255,255,255,0,0,0,0,0,0,0,0]);
-    });
-
-    it('leg_1', function() {
-        ti.leg_1 = 100;
-        expect(ti.values).to.deep.equal([0,0,0,0,8,8,8,8,8,8,0,0,0,0,0,0,0,0]);
-    });
-
-    it('leg_2 (shorthand)', function() {
-        ti.leg_2;
-        expect(ti.values).to.deep.equal([0,0,0,0,0,0,0,0,0,0,255,255,0,255,0,255,255,255]);
-    });
-
-    it('leg_2', function() {
-        ti.leg_2 = 100;
-        expect(ti.values).to.deep.equal([0,0,0,0,0,0,0,0,0,0,8,8,0,8,0,8,8,8]);
-    });
-
-
-
-    it('ring_0 (shorthand)', function() {
-        ti.ring_0;
-        expect(ti.values).to.deep.equal([255,0,0,0,0,0,255,0,0,0,0,0,0,0,0,0,0,255]);
-    });
-    it('ring_0', function() {
-        ti.ring_0 = 100;
-        expect(ti.values).to.deep.equal([8,0,0,0,0,0,8,0,0,0,0,0,0,0,0,0,0,8]);
-    });
-    it('red (shorthand)', function() {
-        ti.red;
-        expect(ti.values).to.deep.equal([255,0,0,0,0,0,255,0,0,0,0,0,0,0,0,0,0,255]);
-    });
-    it('red', function() {
-        ti.red = 100;
-        expect(ti.values).to.deep.equal([8,0,0,0,0,0,8,0,0,0,0,0,0,0,0,0,0,8]);
-    });
-
-
-    it('ring_1 (shorthand)', function() {
-        ti.ring_1;
-        expect(ti.values).to.deep.equal([0,255,0,0,0,0,0,255,0,0,0,0,0,0,0,0,255,0]);
-    });
-    it('ring_1', function() {
-        ti.ring_1 = 100;
-        expect(ti.values).to.deep.equal([0,8,0,0,0,0,0,8,0,0,0,0,0,0,0,0,8,0]);
-    });
-    it('orange (shorthand)', function() {
-        ti.orange;
-        expect(ti.values).to.deep.equal([0,255,0,0,0,0,0,255,0,0,0,0,0,0,0,0,255,0]);
-    });
-    it('orange', function() {
-        ti.orange = 100;
-        expect(ti.values).to.deep.equal([0,8,0,0,0,0,0,8,0,0,0,0,0,0,0,0,8,0]);
-    });
-
-
-    it('ring_2 (shorthand)', function() {
-        ti.ring_2;
-        expect(ti.values).to.deep.equal([0,0,255,0,0,0,0,0,255,0,0,0,0,0,0,255,0,0]);
-    });
-    it('ring_2', function() {
-        ti.ring_2 = 100;
-        expect(ti.values).to.deep.equal([0,0,8,0,0,0,0,0,8,0,0,0,0,0,0,8,0,0]);
-    });
-    it('yellow (shorthand)', function() {
-        ti.yellow;
-        expect(ti.values).to.deep.equal([0,0,255,0,0,0,0,0,255,0,0,0,0,0,0,255,0,0]);
-    });
-    it('yellow', function() {
-        ti.yellow = 100;
-        expect(ti.values).to.deep.equal([0,0,8,0,0,0,0,0,8,0,0,0,0,0,0,8,0,0]);
-    });
-
-
-    it('ring_3 (shorthand)', function() {
-        ti.ring_3;
-        expect(ti.values).to.deep.equal([0,0,0,255,0,255,0,0,0,0,0,0,0,255,0,0,0,0]);
-    });
-    it('ring_3', function() {
-        ti.ring_3 = 100;
-        expect(ti.values).to.deep.equal([0,0,0,8,0,8,0,0,0,0,0,0,0,8,0,0,0,0]);
-    });
-    it('green (shorthand)', function() {
-        ti.green;
-        expect(ti.values).to.deep.equal([0,0,0,255,0,255,0,0,0,0,0,0,0,255,0,0,0,0]);
-    });
-    it('green', function() {
-        ti.green = 100;
-        expect(ti.values).to.deep.equal([0,0,0,8,0,8,0,0,0,0,0,0,0,8,0,0,0,0]);
-    });
-
-
-    it('ring_4 (shorthand)', function() {
-        ti.ring_4;
-        expect(ti.values).to.deep.equal([0,0,0,0,255,0,0,0,0,0,0,255,0,0,255,0,0,0]);
-    });
-    it('ring_4', function() {
-        ti.ring_4 = 100;
-        expect(ti.values).to.deep.equal([0,0,0,0,8,0,0,0,0,0,0,8,0,0,8,0,0,0]);
-    });
-    it('blue (shorthand)', function() {
-        ti.blue;
-        expect(ti.values).to.deep.equal([0,0,0,0,255,0,0,0,0,0,0,255,0,0,255,0,0,0]);
-    });
-    it('blue', function() {
-        ti.blue = 100;
-        expect(ti.values).to.deep.equal([0,0,0,0,8,0,0,0,0,0,0,8,0,0,8,0,0,0]);
-    });
-
-
-    it('ring_5 (shorthand)', function() {
-        ti.ring_5;
-        expect(ti.values).to.deep.equal([0,0,0,0,0,0,0,0,0,255,255,0,255,0,0,0,0,0]);
-    });
-    it('ring_5', function() {
-        ti.ring_5 = 100;
-        expect(ti.values).to.deep.equal([0,0,0,0,0,0,0,0,0,8,8,0,8,0,0,0,0,0]);
-    });
-    it('white (shorthand)', function() {
-        ti.white;
-        expect(ti.values).to.deep.equal([0,0,0,0,0,0,0,0,0,255,255,0,255,0,0,0,0,0]);
-    });
-    it('white', function() {
-        ti.white = 100;
-        expect(ti.values).to.deep.equal([0,0,0,0,0,0,0,0,0,8,8,0,8,0,0,0,0,0]);
-    });
-
-    it('all', function() {
-        ti.all = 100;
-        expect(ti.values).to.deep.equal([8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8]);
-    });
-
-    it('all (shorthand)', function() {
-        ti.all;
-        expect(ti.values).to.deep.equal([255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255]);
-    });
-
-    it('reset', function() {
-        ti.all = 100;
-        expect(ti.values).to.deep.equal([8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8]);
-        ti.reset;
-        expect(ti.values).to.deep.equal([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]);
-    });
-
-    it('should write to the backend', function() {
-        var mock = createBackendMock();
-
-        var ti = piGlowInterface.create(mock);
-        ti.all = 100;
-
-        expect(mock.values).to.deep.equal([[8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8]]);
-    });
-
-    it('should write to the backend (multiple times)', function() {
-        var mock = createBackendMock();
-
-        var ti = piGlowInterface.create(mock);
-        ti.leg_0 = 100;
-        ti.leg_1 = 100;
-        ti.leg_2 = 100;
-
-        expect(mock.values).to.deep.equal([
-            [ 8, 8, 8, 8, 0, 0, 0, 0, 0, 0, 0, 0, 8, 0, 8, 0, 0, 0 ],
-            [ 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 0, 0, 8, 0, 8, 0, 0, 0 ],
-            [ 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8 ]
-        ]);
-    });
-
-    it('should write to the backend (on times)', function() {
-        var mock = createBackendMock();
-
-        var ti = piGlowInterface.create(mock);
-        ti.startTransaction();
-        ti.leg_0 = 100;
-        ti.leg_1 = 100;
-        ti.leg_2 = 100;
-        ti.commitTransaction();
-
-        expect(mock.values).to.deep.equal([
-            [ 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8 ]
-        ]);
-    });
-
-    it('should write to the backend, the predefined stuff (object)', function() {
-        var mock = createBackendMock();
-
-        piGlowInterface.create(mock, {ring_1: 100});
-
-        expect(mock.values).to.deep.equal([[0,8,0,0,0,0,0,8,0,0,0,0,0,0,0,0,8,0]]);
-    });
-
-    it('should write to the backend, the predefined stuff (array)', function() {
-        var mock = createBackendMock();
-
-        piGlowInterface.create(mock, ['ring_2']);
-
-        expect(mock.values).to.deep.equal([[0,0,255,0,0,0,0,0,255,0,0,0,0,0,0,255,0,0]]);
-    });
-
-    it('should set the predefined stuff (object)', function() {
-        var myTi = piGlowInterface.create({ring_0: 100});
-
-        expect(myTi.values).to.deep.equal([8,0,0,0,0,0,8,0,0,0,0,0,0,0,0,0,0,8]);
-    });
-
-    it('should set the predefined stuff (array)', function() {
-        var myTi = piGlowInterface.create(['ring_2']);
-
-        expect(myTi.values).to.deep.equal([0,0,255,0,0,0,0,0,255,0,0,0,0,0,0,255,0,0]);
-    });
-
-    it('should output random data', function() {
-        ti.random = 0.5;
-
-        expect(ti.values).to.satisfy(function(values) {
-            var a = values.reduce(function(memo, value) {
-                if(value > 0) {
-                    memo.a++;
-                } else {
-                    memo.z++;
-                }
-
-                return memo;
-            }, {z:0, a:0});
-
-            return a.z > 0 && a.a > 0;
+    describe('legs', function() {
+        it('leg_0 (shorthand)', function() {
+            ti.leg_0;
+            expect(ti.values).to.deep.equal([255,255,255,255,0,0,0,0,0,0,0,0,255,0,255,0,0,0]);
         });
-    })
 
+        it('leg_0', function() {
+            ti.leg_0 = 100;
+            expect(ti.values).to.deep.equal([8,8,8,8,0,0,0,0,0,0,0,0,8,0,8,0,0,0]);
+        });
+
+        it('leg_1 (shorthand)', function() {
+            ti.leg_1;
+            expect(ti.values).to.deep.equal([0,0,0,0,255,255,255,255,255,255,0,0,0,0,0,0,0,0]);
+        });
+
+        it('leg_1', function() {
+            ti.leg_1 = 100;
+            expect(ti.values).to.deep.equal([0,0,0,0,8,8,8,8,8,8,0,0,0,0,0,0,0,0]);
+        });
+
+        it('leg_2 (shorthand)', function() {
+            ti.leg_2;
+            expect(ti.values).to.deep.equal([0,0,0,0,0,0,0,0,0,0,255,255,0,255,0,255,255,255]);
+        });
+
+        it('leg_2', function() {
+            ti.leg_2 = 100;
+            expect(ti.values).to.deep.equal([0,0,0,0,0,0,0,0,0,0,8,8,0,8,0,8,8,8]);
+        });
+    });
+
+    describe('rings', function() {
+        it('ring_0 (shorthand)', function() {
+            ti.ring_0;
+            expect(ti.values).to.deep.equal([255,0,0,0,0,0,255,0,0,0,0,0,0,0,0,0,0,255]);
+        });
+        it('ring_0', function() {
+            ti.ring_0 = 100;
+            expect(ti.values).to.deep.equal([8,0,0,0,0,0,8,0,0,0,0,0,0,0,0,0,0,8]);
+        });
+        it('red (shorthand)', function() {
+            ti.red;
+            expect(ti.values).to.deep.equal([255,0,0,0,0,0,255,0,0,0,0,0,0,0,0,0,0,255]);
+        });
+        it('red', function() {
+            ti.red = 100;
+            expect(ti.values).to.deep.equal([8,0,0,0,0,0,8,0,0,0,0,0,0,0,0,0,0,8]);
+        });
+
+
+        it('ring_1 (shorthand)', function() {
+            ti.ring_1;
+            expect(ti.values).to.deep.equal([0,255,0,0,0,0,0,255,0,0,0,0,0,0,0,0,255,0]);
+        });
+        it('ring_1', function() {
+            ti.ring_1 = 100;
+            expect(ti.values).to.deep.equal([0,8,0,0,0,0,0,8,0,0,0,0,0,0,0,0,8,0]);
+        });
+        it('orange (shorthand)', function() {
+            ti.orange;
+            expect(ti.values).to.deep.equal([0,255,0,0,0,0,0,255,0,0,0,0,0,0,0,0,255,0]);
+        });
+        it('orange', function() {
+            ti.orange = 100;
+            expect(ti.values).to.deep.equal([0,8,0,0,0,0,0,8,0,0,0,0,0,0,0,0,8,0]);
+        });
+
+
+        it('ring_2 (shorthand)', function() {
+            ti.ring_2;
+            expect(ti.values).to.deep.equal([0,0,255,0,0,0,0,0,255,0,0,0,0,0,0,255,0,0]);
+        });
+        it('ring_2', function() {
+            ti.ring_2 = 100;
+            expect(ti.values).to.deep.equal([0,0,8,0,0,0,0,0,8,0,0,0,0,0,0,8,0,0]);
+        });
+        it('yellow (shorthand)', function() {
+            ti.yellow;
+            expect(ti.values).to.deep.equal([0,0,255,0,0,0,0,0,255,0,0,0,0,0,0,255,0,0]);
+        });
+        it('yellow', function() {
+            ti.yellow = 100;
+            expect(ti.values).to.deep.equal([0,0,8,0,0,0,0,0,8,0,0,0,0,0,0,8,0,0]);
+        });
+
+
+        it('ring_3 (shorthand)', function() {
+            ti.ring_3;
+            expect(ti.values).to.deep.equal([0,0,0,255,0,255,0,0,0,0,0,0,0,255,0,0,0,0]);
+        });
+        it('ring_3', function() {
+            ti.ring_3 = 100;
+            expect(ti.values).to.deep.equal([0,0,0,8,0,8,0,0,0,0,0,0,0,8,0,0,0,0]);
+        });
+        it('green (shorthand)', function() {
+            ti.green;
+            expect(ti.values).to.deep.equal([0,0,0,255,0,255,0,0,0,0,0,0,0,255,0,0,0,0]);
+        });
+        it('green', function() {
+            ti.green = 100;
+            expect(ti.values).to.deep.equal([0,0,0,8,0,8,0,0,0,0,0,0,0,8,0,0,0,0]);
+        });
+
+
+        it('ring_4 (shorthand)', function() {
+            ti.ring_4;
+            expect(ti.values).to.deep.equal([0,0,0,0,255,0,0,0,0,0,0,255,0,0,255,0,0,0]);
+        });
+        it('ring_4', function() {
+            ti.ring_4 = 100;
+            expect(ti.values).to.deep.equal([0,0,0,0,8,0,0,0,0,0,0,8,0,0,8,0,0,0]);
+        });
+        it('blue (shorthand)', function() {
+            ti.blue;
+            expect(ti.values).to.deep.equal([0,0,0,0,255,0,0,0,0,0,0,255,0,0,255,0,0,0]);
+        });
+        it('blue', function() {
+            ti.blue = 100;
+            expect(ti.values).to.deep.equal([0,0,0,0,8,0,0,0,0,0,0,8,0,0,8,0,0,0]);
+        });
+
+
+        it('ring_5 (shorthand)', function() {
+            ti.ring_5;
+            expect(ti.values).to.deep.equal([0,0,0,0,0,0,0,0,0,255,255,0,255,0,0,0,0,0]);
+        });
+        it('ring_5', function() {
+            ti.ring_5 = 100;
+            expect(ti.values).to.deep.equal([0,0,0,0,0,0,0,0,0,8,8,0,8,0,0,0,0,0]);
+        });
+        it('white (shorthand)', function() {
+            ti.white;
+            expect(ti.values).to.deep.equal([0,0,0,0,0,0,0,0,0,255,255,0,255,0,0,0,0,0]);
+        });
+        it('white', function() {
+            ti.white = 100;
+            expect(ti.values).to.deep.equal([0,0,0,0,0,0,0,0,0,8,8,0,8,0,0,0,0,0]);
+        });
+    });
+
+    describe('all', function() {
+        it('all', function() {
+            ti.all = 100;
+            expect(ti.values).to.deep.equal([8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8]);
+        });
+
+        it('all (shorthand)', function() {
+            ti.all;
+            expect(ti.values).to.deep.equal([255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255]);
+        });
+
+        it('reset', function() {
+            ti.all = 100;
+            expect(ti.values).to.deep.equal([8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8]);
+            ti.reset;
+            expect(ti.values).to.deep.equal([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]);
+        });
+    });
+
+    describe('backend', function() {
+        it('should write to the backend', function() {
+            var mock = createBackendMock();
+
+            var ti = piGlowInterface(mock);
+            ti.all = 100;
+
+            expect(mock.values).to.deep.equal([[8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8]]);
+        });
+
+        it('should write to the backend (multiple times)', function() {
+            var mock = createBackendMock();
+
+            var ti = piGlowInterface(mock);
+            ti.leg_0 = 100;
+            ti.leg_1 = 100;
+            ti.leg_2 = 100;
+
+            expect(mock.values).to.deep.equal([
+                [ 8, 8, 8, 8, 0, 0, 0, 0, 0, 0, 0, 0, 8, 0, 8, 0, 0, 0 ],
+                [ 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 0, 0, 8, 0, 8, 0, 0, 0 ],
+                [ 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8 ]
+            ]);
+        });
+
+        it('should write to the backend (one time)', function() {
+            var mock = createBackendMock();
+
+            var ti = piGlowInterface(mock);
+            ti.startTransaction();
+            ti.leg_0 = 100;
+            ti.leg_1 = 100;
+            ti.leg_2 = 100;
+            ti.commitTransaction();
+
+            expect(mock.values).to.deep.equal([
+                [ 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8 ]
+            ]);
+        });
+
+        //TODO: add tests for mulitple transaction levels
+    });
+
+    describe('predefined', function() {
+        it('should write to the backend, the predefined stuff (object)', function() {
+            var mock = createBackendMock();
+
+            piGlowInterface(mock, {ring_1: 100});
+
+            expect(mock.values).to.deep.equal([[0,8,0,0,0,0,0,8,0,0,0,0,0,0,0,0,8,0]]);
+        });
+
+        it('should write to the backend, the predefined stuff (array)', function() {
+            var mock = createBackendMock();
+
+            piGlowInterface(mock, ['ring_2']);
+
+            expect(mock.values).to.deep.equal([[0,0,255,0,0,0,0,0,255,0,0,0,0,0,0,255,0,0]]);
+        });
+
+        it('should set the predefined stuff (object)', function() {
+            var myTi = piGlowInterface({ring_0: 100});
+
+            expect(myTi.values).to.deep.equal([8,0,0,0,0,0,8,0,0,0,0,0,0,0,0,0,0,8]);
+        });
+
+        it('should set the predefined stuff (array)', function() {
+            var myTi = piGlowInterface(['ring_2']);
+
+            expect(myTi.values).to.deep.equal([0,0,255,0,0,0,0,0,255,0,0,0,0,0,0,255,0,0]);
+        });
+    });
+
+    describe('random', function() {
+        it('should output random data', function() {
+            ti.random = 0.5;
+
+            expect(ti.values).to.satisfy(function(values) {
+                var a = values.reduce(function(memo, value) {
+                    if(value > 0) {
+                        memo.a++;
+                    } else {
+                        memo.z++;
+                    }
+
+                    return memo;
+                }, {z:0, a:0});
+
+                return a.z > 0 && a.a > 0;
+            });
+        });
+    });
 });
