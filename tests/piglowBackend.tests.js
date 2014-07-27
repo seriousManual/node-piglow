3103a2f0e7e2e8a501636e1b9f6b8bc2ca150716
zaphod1984 <zaphod84@gmx.de>
2014-07-27 18:50:59 +0200

    only changes should be written to the piglow
    if a write order gets assigned to the backend it makes sure that the
    assigned values are actually changes and discards them if not.

--- tests/piglowBackend.tests.js
+++ tests/piglowBackend.tests.js
@@ -77,6 +77,44 @@ describe('piGlowBackend', function () {
             });
     });
 
+    it('should initialize and write bytes, should remap values only once', function (done) {
+        var Backend = sand.require('../lib/PiGlowBackend', {
+            requires: {
+                'i2c': createI2cMock()
+            }
+        });
+
+        new Backend()
+            .on('initialize', function () {
+                var that = this;
+
+                that.update({
+                    l_0_0: 100, l_0_1: 101, l_0_2: 102, l_0_3: 103, l_0_4: 104, l_0_5: 105,
+                    l_1_0: 150, l_1_1: 151, l_1_2: 152, l_1_3: 153, l_1_4: 154, l_1_5: 155,
+                    l_2_0: 200, l_2_1: 201, l_2_2: 202, l_2_3: 203, l_2_4: 204, l_2_5: 205
+                }, function () {
+                    that.update({
+                        l_0_0: 100, l_0_1: 101, l_0_2: 102, l_0_3: 103, l_0_4: 104, l_0_5: 105,
+                        l_1_0: 150, l_1_1: 151, l_1_2: 152, l_1_3: 153, l_1_4: 154, l_1_5: 155,
+                        l_2_0: 200, l_2_1: 201, l_2_2: 202, l_2_3: 203, l_2_4: 204, l_2_5: 205
+                    }, function() {
+                        var data = that._wire;
+
+                        expect(data._addr).to.equal(84);
+                        expect(data._options).to.deep.equal({device: '/dev/i2c-1'});
+                        expect(data._written).to.deep.equal([
+                            [0, [1]],
+                            [19, [255, 255, 255]],
+                            [1, [8, 8, 9, 9, 28, 27, 26, 26, 27, 29, 86, 84, 9, 82, 9, 80, 79, 77 ]],
+                            [22, [255]]
+                        ]);
+
+                        done();
+                    });
+                });
+            });
+    });
+
     it('should throw', function (done) {
         var Backend = sand.require('../lib/PiGlowBackend', {
             requires: {
